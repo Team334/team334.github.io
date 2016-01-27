c8b8d7df8d079d5d7670b1eb7b83fb11da362e30
Kevin <kevtehpro35@gmail.com>
2016-01-27 16:47:16 -0500

    Added tabs for slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
     var slideshow = $("#slideshow");
 
     setInterval(function() {
-        var active = $(".active.slide");
-        var next = (active.next().length > 0) ? active.next() : $("#slideshow img:first");
+        var activeSlide = $(".active.slide");
+        var next = (activeSlide.next().length > 0) ? activeSlide.next() : $("#slideshow img:first");
         next.css("z-index", 2);
-        active.fadeOut(1500, function() {
-            active.css("z-index", 1).show().removeClass("active");
+        activeSlide.fadeOut(1500, function() {
+            activeSlide.css("z-index", 1).show().removeClass("active");
             next.css("z-index", 3).addClass("active");
         });
+
+        $("ul.tabs").tabs('select_tab', 'pic2')
+
     }, 7000);
 
+    $("ul.tabs").tabs();
 });
