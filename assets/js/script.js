a7d98ec31aaa025adc3c52f70985370d9db9fbe9
Kevin <kevtehpro35@gmail.com>
2016-01-28 11:49:01 -0500

    50% working tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,44 @@ $(document).ready(function() {
     var index = 0;
     var max_index = 5;
 
-    function cycleToImage() {
+    function cycleImage() {
         var slides = $(".slide");
-        index = next(index);
         var currSlide = $(".active.slide"); 
+        index = next(index);
         var nextSlide = $(slides[next(index)]); 
-
 
         nextSlide.css("z-index", 2);
         currSlide.fadeOut(200, function() {
-        currSlide.css("z-index", 1).show().removeClass("active");
-        nextSlide.css("z-index", 3).addClass("active");
+            currSlide.css("z-index", 1).show().removeClass("active");
+            nextSlide.css("z-index", 3).addClass("active");
         }); 
-        $('#tabs').tabs('select_tab', "pic" + (index + 1));
     }
 
     function next(i) {
         return i + 1 == max_index ? 0 : i + 1;
     }
 
-    setInterval(function() {
-        cycleToImage(index);
-    }, 7000);
+    function prev(i) {
+        return i - 1 == -1 ? 4 : i - 1;
+    }
+
+    var interval;
+
+    function resetInterval() {
+        clearInterval(interval);
+        interval = setInterval(function() {
+            $("ul.tabs").tabs("select_tab", "pic" + (next(index) + 1));
+        }, 2000);
+    }
+    resetInterval();
+
+    $("li.tab.cols3 > a").click(function() {
+        if (parseInt(this.innerHTML) != index) {
+            index = prev(parseInt(this.innerHTML));
+            cycleImage();
+        }
+        resetInterval();
+    });
 
     $("ul.tabs").tabs();
 });
