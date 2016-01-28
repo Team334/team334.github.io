3b749af58a81a3b2c6058edb6f4d891ad963e0c1
Kevin <kevtehpro35@gmail.com>
2016-01-27 19:46:57 -0500

    Finished slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
     var slideshow = $("#slideshow");
+    var index = 0;
+    var max_index = 5;
+
+    function cycleToImage() {
+        var slides = $(".slide");
+        index = next(index);
+        var currSlide = $(".active.slide"); 
+        var nextSlide = $(slides[next(index)]); 
+
+
+        nextSlide.css("z-index", 2);
+        currSlide.fadeOut(200, function() {
+        currSlide.css("z-index", 1).show().removeClass("active");
+        nextSlide.css("z-index", 3).addClass("active");
+        }); 
+        $('#tabs').tabs('select_tab', "pic" + (index + 1));
+    }
+
+    function next(i) {
+        return i + 1 == max_index ? 0 : i + 1;
+    }
 
     setInterval(function() {
-        var activeSlide = $(".active.slide");
-        var next = (activeSlide.next().length > 0) ? activeSlide.next() : $("#slideshow img:first");
-        next.css("z-index", 2);
-        activeSlide.fadeOut(1500, function() {
-            activeSlide.css("z-index", 1).show().removeClass("active");
-            next.css("z-index", 3).addClass("active");
-        });
-
-        $("ul.tabs").tabs('select_tab', 'pic2')
-
+        cycleToImage(index);
     }, 7000);
 
     $("ul.tabs").tabs();
