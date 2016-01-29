8acd68ae6c9d3f16d7b5630abb9f2c3e4236e416
Kevin <kevtehpro35@gmail.com>
2016-01-29 13:58:06 -0500

    Fixed speed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,10 +41,7 @@ $(document).ready(function() {
         if (index != parseInt(this.innerHTML) - 1) {
             cycleImage(parseInt(this.innerHTML) - 1);
             index = parseInt(this.innerHTML) - 1;
-        } else {
-            clearInterval(interval);
-            setTimeout(resetInterval, 3000);
-        }
+        } 
     });
 
     $("ul.tabs").tabs();
