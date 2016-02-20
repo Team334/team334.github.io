b42533f2bb3e0310c6ab85bd261c9bd21e81945c
Kevin <kevtehpro35@gmail.com>
2016-02-20 18:15:25 -0500

    Forgot a member on the about us and also worked on mobile

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $(".fa.fa-bars").click(function(event) {
+        setTimeout(function() {
+            $("#sidebar").show();
+        }, 1);
+    });
+
+    $("#content").click(function(event) {
+        if ($(".fa.fa-bars").css("display") == "inline" && $("#sidebar").css("display") == "block") {
+            $("#sidebar").hide();
+        }
+    });
+
+});
