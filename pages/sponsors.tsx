7bcb88c8c5a0ca1e4899726fba94a0333881edf1
Jerry <zeruiz@nycstudents.net>
2024-06-09 16:48:37 -0400

    New Sponsors page

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,49 +1,45 @@
-"use client"
-import {AuroraBackground} from "@/components/aceternity/ui/aurora";
-import {motion} from "framer-motion";
-import {Image} from "@nextui-org/react";
+import Link from "next/link";
 import React from "react";
-import {BackgroundGradient} from "@/components/aceternity/ui/bg-gradient-card";
-import {Link} from "@nextui-org/link";
-
+import Image from "next/image";
+import {Button} from "@/components/shadcn/ui/button";
 
 export default function SponsorPage() {
-    const bentoItems = [
+    const sponsors = [
         {
             image: "/logos/first.png",
             title: "FIRST",
             url: "https://www.firstinspires.org/",
-            description: ""
+            description: "FIRST is a global robotics community preparing young people for the future and the world's leading youth-serving nonprofit advancing STEM education."
         },
         {
             image: "/sponsors/alumni-foundation.png",
             title: "Brooklyn Tech Alumni Foundation",
-            url: "",
-            description: ""
+            url: "https://bthsalumni.org/",
+            description: "The Brooklyn Tech Alumni Foundation mobilizes Tech graduates to support the nation's premier high school for science, technology, engineering and mathematics"
         },
         {
             image: "/sponsors/arament.png",
             title: "Arament Research, Development and Engineering Center",
             url: "",
-            description: ""
+            description: "The U.S. Army Armament Research, Development and Engineering Center (ARDEC) is an internationally acknowledged hub for the advancement of armament technologies"
         },
         {
             image: "/sponsors/con-edison.png",
             title: "Con Edison",
             url: "https://www.coned.com/en",
-            description: ""
+            description: "We operate one of the world's largest energy delivery systems. Founded in 1823 as the New York Gas Light company, our electric, gas, and steam service now"
         },
         {
             image: "/sponsors/dodstem.png",
             title: "DoD STEM",
             url: "https://www.dodstem.us",
-            description: ""
+            description: "Science, Technology, Engineering and Mathematics, or STEM, is the gateway to a world of wonder. U.S. Department of Defense (DoD) STEM professionals work at the leading edge of our nation’s most advanced technological breakthroughs. STEM is our future."
         },
         {
             image: "/sponsors/haas-foundation.png",
             title: "Gene HAAS Foundation",
             url: "https://ghaasfoundation.org/content/ghf/en/home.html",
-            description: ""
+            description: "The Gene Haas Foundation was established in 1999, by Gene Haas, founder and owner of Haas Automation, Inc., to support the needs of the local community, through grants to such local charities as the Boys and Girls Clubs, Food Share, Rescue Mission, and others."
         },
         {
             image: "/sponsors/ike-heller.png",
@@ -55,67 +51,49 @@ export default function SponsorPage() {
             image: "/sponsors/quotebeam.png",
             title: "QuoteBeam",
             url: "https://quotebeam.com",
-            description: ""
+            description: "We aim to eliminate frustrations in the purchasing process by streamlining finding product information, getting technical support, quotations, and ordering. All in one place."
         },
         {
             image: "/sponsors/whimsy-tech.png",
             title: "Whimsy Tech",
             url: "https://whimsytech.com",
-            description: ""
+            description: "#TeamWhimsy"
         }
     ];
 
-
     return (
-        <div className={"w-full"}>
-            <AuroraBackground>
-                <motion.div
-                    initial={{opacity: 0.0, y: 40}}
-                    whileInView={{opacity: 1, y: 0}}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="relative flex flex-col gap-4 items-center justify-center px-4 text-center"
-                >
-                    <div className="text-4xl md:text-7xl font-bold text-white text-center main">
-                        Our Sponsors
+        <div className="p-8 text-center">
+            <div className="text-4xl md:text-7xl font-bold text-white text-center main">
+                Our Sponsors
+            </div>
+            <div className="font-extralight text-base md:text-4xl text-neutral-200 py-4">
+                This page is dedicated to our all of sponsors! We couldn't make our robot without your help!
+            </div>
+            <div className="space-y-8">
+                {sponsors.map((sponsor, index) => (
+                    <div key={index}
+                         className="flex flex-col md:flex-row items-center justify-center md:justify-start space-y-5 md:space-y-0 md:space-x-4 h-auto md:h-64">
+                        <Image
+                            src={sponsor.image}
+                            alt={sponsor.title}
+                            width={158}
+                            height={158}
+                            className="md:ml-[8rem]"
+                        />
+                        <div className="flex flex-col justify-center items-center align-middle w-full">
+                            <h2 className="text-[2.1rem] secondary underline">{sponsor.title}</h2>
+                            <p className="mb-2 text-lg text-slate-300">{sponsor.description}</p>
+                            {sponsor.url && (
+                                <Link href={sponsor.url} target="_blank">
+                                    <Button
+                                        className="inline-block px-4 py-2 rounded-xl bg-gray-200 w-64 mt-5 text-black hover:bg-gray-400">
+                                        Learn more
+                                    </Button>
+                                </Link>
+                            )}
+                        </div>
                     </div>
-                    <div className="font-extralight text-base md:text-4xl text-neutral-200 py-4">
-                        This page is dedicated to our all of sponsors! We couldn't make our robot without your help!
-                    </div>
-                </motion.div>
-            </AuroraBackground>
-            <div className={"h-full py-20 items-center justify-center"}>
-                <div className="flex flex-wrap justify-center gap-5">
-                    {bentoItems.map((item, i) => (
-                        <BackgroundGradient
-                            className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-zinc-900 flex flex-col justify-between"
-                            key={i}>
-                            <div className="h-[400px] w-[300px] flex flex-col justify-between">
-                                <div className="flex items-center justify-center h-[300px] overflow-hidden">
-                                    <Image
-                                        src={item.image}
-                                        alt="sponsor"
-                                        width={300}
-                                        height={300}
-                                        className="max-w-full max-h-full object-contain object-center"
-                                    />
-                                </div>
-                                <div className="mt-4">
-                                    <Link className="text-base sm:text-xl text-neutral-200"
-                                          href={item.url}>
-                                        {item.title}
-                                    </Link>
-                                    <p className="text-sm text-neutral-400">
-                                        {item.description}
-                                    </p>
-                                </div>
-                            </div>
-                        </BackgroundGradient>
-                    ))}
-                </div>
+                ))}
             </div>
         </div>
     );
