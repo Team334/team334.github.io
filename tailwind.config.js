3a4dd4c87a18efe541493520872d0efa204397ab
Jerry <zeruiz@nycstudents.net>
2024-06-15 15:25:13 -0400

    Bump and update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ const config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   prefix: "",
   theme: {
