ed3a37b0d2f0f669217523ffd7acbc61b3cbd1d3
Jerry <zeruiz@nycstudents.net>
2024-10-11 14:44:52 -0400

    Revert

---
+++ components/MemberCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+
+export type Member = {
+    name: string;
+    department: string;
+    image: string;
+}
+
+interface MemberProps {
+    member: Member;
+}
+
+export const MemberCard: React.FC<MemberProps> = ({ member }) => (
+    <div className="w-36 flex flex-col">
+        <div className="relative w-full h-36 mb-3">
+            <Image loader={({ src }) => src} src={member.image} alt={member.name} objectFit="cover" layout="fill" className="rounded-full" />
+        </div>
+        <p className="text-center mb-0">{member.name}</p>
+        {/*<p className="capitalize text-center">{member.department}</p>*/}
+    </div>
+);
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,130 +1,70 @@
-import {notFound} from "next/navigation";
-import React from "react";
-import Image from "next/image";
-import markdownStyles from "@/styles/markdown-styles.module.css";
-import remarkGfm from "remark-gfm";
-import ReactMarkdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
-import remarkParse from "remark-parse";
-import remarkStringify from "remark-stringify";
-import remarkRehype from "remark-rehype";
-import rehypeFormat from "rehype-format";
-import rehypeMinifyWhitespace from "rehype-minify-whitespace";
-import rehypeStringify from "rehype-stringify";
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import fs from 'fs';
+import path from 'path';
+import slugify from 'slugify';
+import matter from 'gray-matter';
 
-// import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter'
-// import {prism} from 'react-syntax-highlighter/dist/esm/styles/prism'
-//
-// import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
-// import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
-// import py from 'react-syntax-highlighter/dist/esm/languages/prism/python';
-// import html from 'react-syntax-highlighter/dist/esm/languages/prism/markup';
-// import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
-// import json from 'react-syntax-highlighter/dist/esm/languages/prism/json';
-// import java from 'react-syntax-highlighter/dist/esm/languages/prism/java';
-// import dart from 'react-syntax-highlighter/dist/esm/languages/prism/dart';
-import {GetStaticPaths, GetStaticProps} from "next";
-import {getPostBySlug, getPostSlugs, Post} from "@/components/markdown";
-// SyntaxHighlighter.registerLanguage('tsx', tsx);
-// SyntaxHighlighter.registerLanguage('js', js);
-// SyntaxHighlighter.registerLanguage('py', py);
-// SyntaxHighlighter.registerLanguage('html', html);
-// SyntaxHighlighter.registerLanguage('css', css);
-// SyntaxHighlighter.registerLanguage('json', json);
-// SyntaxHighlighter.registerLanguage('java', java);
-// SyntaxHighlighter.registerLanguage('dart', dart);
-
-
-type PostPageProps = {
-    post: Post | undefined;
-};
-
-const PostPage: React.FC<PostPageProps> = ({ post }) => {
-    if (!post) {
-        return notFound();
-    }
-
-    return (
-        <div className="p-10">
-            <div className={"flex justify-center items-center text-center w-full my-10"}>
-                <div className={"space-y-5"}>
-                    <h1 className={"text-[2.9rem] md:text-7xl font-bold text-white secondary p-2"}>{post.title}</h1>
-                    <div className="flex flex-row items-center justify-center text-center gap-2">
-                        <Image
-                            alt="blogAuthor"
-                            src={post.author.picture}
-                            className="rounded-full"
-                            width={40}
-                            height={40}
-                            style={{width: "40px", height: "40px", objectFit: "cover"}}
-                        />
-
-                        <h1 className="main">{post.author.name}</h1>
-                        <div className="border-l border-gray-300 h-6 mx-2"/>
-                        <h1 className="secondary">{new Date(post.date).toLocaleDateString()}</h1>
-                    </div>
-                    <Image
-                        src={post.coverImage}
-                        alt={"blogImage"}
-                        height={"800"}
-                        width={"1500"}
-                    />
-                </div>
-            </div>
-            <div className="prose max-w-none prose-invert">
-
-                <ReactMarkdown
-                    className={markdownStyles["markdown"]}
-                    remarkPlugins={[remarkGfm, remarkParse, remarkStringify, remarkRehype]}
-                    rehypePlugins={[rehypeRaw, rehypeFormat, rehypeMinifyWhitespace, rehypeStringify]}
-                    // components={{
-                    //     code({node, inline, className, children, ...props}: any) {
-                    //         const match = /language-(\w+)/.exec(className || '');
-                    //
-                    //         return !inline && match ? (
-                    //             <SyntaxHighlighter style={prism} PreTag="div" language={match[1]} {...props}>
-                    //                 {String(children).replace(/\n$/, '')}
-                    //             </SyntaxHighlighter>
-                    //         ) : (
-                    //             <code className={className} {...props}>
-                    //                 {children}
-                    //             </code>
-                    //         );
-                    //     },
-                    // }}
-                >
-                    {post.content}
-                </ReactMarkdown>
-            </div>
-        </div>
-    );
+interface BlogPostParams extends ParsedUrlQuery {
+    slug: string;
 }
 
-type Params = {
-    params: {
-        slug: string;
-    };
+interface BlogPostProps {
+    slug: string;
+    frontMatter: { [key: string]: any },
+    content: string,
+}
+
+const BlogPost: NextPage<BlogPostProps> = ({ slug, frontMatter, content }) => (
+    <div className="container py-36">
+        <div className="relative h-72 w-full rounded-[1.5rem] overflow-hidden mb-10">
+            {frontMatter.cover && (
+                <Image
+                    loader={({ src }) => src}
+                    src={`/blog/${frontMatter.cover.filename}`}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition={frontMatter.cover.position}
+                />
+            )}
+            <div className="relative w-full h-full flex bg-gradient-to-b from-transparent via-slate-900/70 to-slate-900 px-16 py-10">
+                <div className="self-end">
+                    <h1 className="text-5xl">{frontMatter.title}</h1>
+                    <h4 className="font-normal">by {frontMatter.author} — {new Date(frontMatter.posted).toLocaleDateString()}</h4>
+                </div>
+            </div>
+        </div>
+        <article className="prose max-w-none dark:prose-invert">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        </article>
+    </div>
+);
+
+export const getStaticPaths: GetStaticPaths<BlogPostParams> = async () => {
+    const files = fs.readdirSync(path.join('posts'));
+    const paths = files.map((filename) => ({
+        params: {
+            slug: slugify(filename).replace('.md', ''),
+        },
+    }));
+
+    return { paths, fallback: false };
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-    const paths = getPostSlugs().map((slug) => ({ params: { slug: slug.replace(/\.md$/, '') } }));
-    return {
-        paths,
-        fallback: false,
-    };
-};
-
-export const getStaticProps: GetStaticProps<{ post: Post }> = async ({
-                                                                         params,
-                                                                     }) => {
-    const { slug } = params as Params["params"];
-    const post = getPostBySlug(slug);
+export const getStaticProps: GetStaticProps<BlogPostProps, BlogPostParams> = async ({ params }) => {
+    const markdownWithMeta = fs.readFileSync(path.join('posts', `${params!.slug}.md`), 'utf-8');
+    const { data, content } = matter(markdownWithMeta);
 
     return {
         props: {
-            post,
-        },
+            frontMatter: JSON.parse(JSON.stringify(data)),
+            slug: params!.slug,
+            content,
+        }
     };
 };
 
-export default PostPage;
+export default BlogPost;
